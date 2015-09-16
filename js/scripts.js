5db4019fa44159696ae1a4343a69ab7b02c88a63
Molly Waggett & Jennifer Morkunas <student@epicodus.com>
2015-09-16 09:48:22 -0700

    pass specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,22 @@
-// var functionName = function(params) {
-//
-// };
 
-// $(document).ready(function() {
-//
-// });
+var countUpBy = function(increment, max) {
+  var output = [];
+  for (var i = increment; i <= max; i += increment) {
+    output.push(i);
+  }
+  return output;
+};
+
+$(document).ready(function() {
+  $("form#getNumber").submit(function(event) {
+    var max = parseInt($("input#number").val());
+    var increment = parseInt($("input#multiple").val());
+
+    var result = countUpBy(increment, max);
+
+    $(".output").text(result);
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,9 @@
-// describe('functionName', function() {
-//   it("plain English description of spec", function() {
-//     expect(functionName(argument)).to.equal(value);
-//   });
-// });
+describe('countUpBy', function() {
+  it("counts up to one number by another number", function() {
+    expect(countUpBy(5,30)).to.eql([5,10,15,20,25,30]);
+  });
+
+  it("counts up to one number by another number", function() {
+    expect(countUpBy(7,50)).to.eql([7,14, 21, 28, 35, 42, 49]);
+  });
+});
